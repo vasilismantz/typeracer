0c276af75c4be16509db73ac33991b801577a4cb
Vasilis Mantzaris <mantzaris.vas98@gmail.com>
2021-02-08 14:12:09 +0100

    [i-5] :recycle: Transfer routes to their own files

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
 import connect from "./lib/mongoose";
-
+import indexRouter from "./routes/index";
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
@@ -14,68 +13,7 @@ app.use(bodyParser.json());
   await connect();
 })();
 
-const users = [
-  {
-    username: "john",
-    password: "password123admin",
-    role: "admin",
-  },
-  {
-    username: "anna",
-    password: "password123member",
-    role: "member",
-  },
-];
-
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  const user = users.find((u) => {
-    return u.username === username && u.password === password;
-  });
-
-  if (user) {
-    const token = jwt.sign(
-      { username: user.username, role: user.role },
-      process.env.JWT_SECRET
-    );
-    res.status(200).json({
-      data: {
-        token,
-      },
-    });
-  } else {
-    res.status(401).json({
-      error: {
-        msg: "Incorrect username or password",
-      },
-    });
-  }
-});
-
-app.post("/register", (req, res) => {
-  const { username, password } = req.body;
-
-  const userExists = users.find((u) => {
-    return u.username === username;
-  });
-
-  if (!userExists) {
-    //TO DO
-    //Add user to db
-    res.status(200).json({
-      data: {
-        msg: "Successfully registered",
-      },
-    });
-  } else {
-    res.status(401).json({
-      error: {
-        msg: "Username already exists",
-      },
-    });
-  }
-});
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
